1b7b01e8256a04fe847e89a64cd819507985bc23
kimberlytatiana11822-netizen <kimberlytatiana11822-netizen@users.noreply.github.com>
2026-07-30 05:05:30 -0300

    fix: pagina principal dinamica para mostrar productos nuevos

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { client } from '@/sanity/lib/client'
 import CatalogView from './CatalogView'
 import type { Product } from './types'
 
+export const dynamic = 'force-dynamic'
+
 async function getProducts(): Promise<Product[]> {
   const query = `*[_type == "product"]{
     _id,
